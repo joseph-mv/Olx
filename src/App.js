4700bab7eb2e3c772bceb09d8f80afadd284aa63
josephmv43 <josephmv43@gmail.com>
2024-07-11 12:13:14 +0530

    browserRouter to Hash

--- src/App.js
+++ src/App.js
@@ -1,52 +1,45 @@
 import "./App.css";
 import Home from "./Pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./Pages/Signup";
 import Login from "./Pages/Login";
 import Create from "./Pages/Create";
-import ViewPost from "./Pages/ViewPost"
+import ViewPost from "./Pages/ViewPost";
 
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "./store/FirebaseContext";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import{Post} from './store/PostContext'
+import { Post } from './store/PostContext';
 
 function App() {
   const { setUser } = useContext(AuthContext);
-  
- 
+
   useEffect(() => {
     const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
       if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/auth.user
         const uid = user.uid;
-        // ...
         setUser(user);
       } else {
         // User is signed out
-        // ...
       }
     });
   }, []);
- 
+
   return (
-    
     <div>
-      <Post >
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/sell" element={<Create />}></Route>
-          <Route path="/view" element={<ViewPost/>}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Post>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/sell" element={<Create />} />
+            <Route path="/view" element={<ViewPost />} />
+          </Routes>
+        </Router>
       </Post>
     </div>
-    
   );
 }
 
